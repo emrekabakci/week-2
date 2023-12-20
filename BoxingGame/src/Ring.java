dc4ddb431b77e0365b641068bf294a5d23eea95d
emrekabakci <221238040@ogrenci.karatay.edu.tr>
2023-12-20 23:20:10 +0300

    BoxingGame completed.

---
+++ BoxingGame/src/Ring.java
@@ -0,0 +1,74 @@
+import java.util.*;
+public class Ring {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    public Ring(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public void run() {
+
+        if (checkWeight()) {
+            boolean whichIsFirst = new Random().nextBoolean();
+
+            if (whichIsFirst){
+                System.out.println("Maça İlk " +this.f1.name + " Başlıyor!");
+            }else {
+                System.out.println("Maça İlk " +this.f2.name + " Başlıyor!");
+            }
+            while (this.f1.health > 0 && this.f2.health > 0) {
+                printScore();
+                System.out.println("-----YENİ ROUND-----");
+
+                if (whichIsFirst) {
+                    this.f2.health = this.f1.hit(this.f2);
+                    if (isWin())
+                        break;
+
+                    this.f1.health = this.f2.hit(this.f1);
+                } else {
+                    this.f1.health = this.f2.hit(this.f1);
+                    if (isWin())
+                        break;
+
+                    this.f2.health = this.f1.hit(this.f2);
+                }
+                if (isWin())
+                    break;
+            }
+        } else {
+            System.out.println("Sporcuların ağırlıkları uyuşmuyor.");
+        }
+
+
+    }
+
+    public boolean checkWeight() {
+        return (f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight);
+    }
+
+    public boolean isWin() {
+        if (f1.health == 0) {
+            System.out.println("Maçı Kazanan : " + f2.name);
+            return true;
+        } else if (f2.health == 0){
+            System.out.println("Maçı Kazanan : " + f1.name);
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void printScore() {
+        System.out.println("------------");
+        System.out.println(f1.name + " Kalan Can \t:" + f1.health);
+        System.out.println(f2.name + " Kalan Can \t:" + f2.health);
+    }
+}
